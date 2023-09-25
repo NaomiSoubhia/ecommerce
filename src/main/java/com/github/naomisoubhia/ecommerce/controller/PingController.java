00380eb1a1e03df1fd60da2f8a18b825f883e5b8
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2023-09-25 08:17:51 -0300

    mensagem

--- src/main/java/com/github/naomisoubhia/ecommerce/controller/PingController.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/controller/PingController.java
@@ -5,11 +5,13 @@ import java.util.Calendar;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @RestController
 @RequestMapping
 public class PingController {
-
+	
 	@Value("${mensagem}")
 	private String mensagem;
 
