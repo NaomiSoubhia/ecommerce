e23e8a0ce220f93e52e415b6a8df8452560e05d3
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2023-09-25 09:43:31 -0300

    teste

--- src/main/java/com/github/naomisoubhia/ecommerce/controller/PingController.java
+++ src/main/java/com/github/naomisoubhia/ecommerce/controller/PingController.java
@@ -2,23 +2,25 @@ package com.github.naomisoubhia.ecommerce.controller;
 
 import java.util.Calendar;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.convert.ValueConverter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.core.env.Environment;
 
 @RestController
 @RequestMapping
 public class PingController {
 	
 
-	@Value("${mensagem}")
-	 private String mens;
+	 @Autowired
+	    private Environment environment;
 
-    @GetMapping("/ping")
-    public String ping() {
-        return "ping pong: " + mens;
-    }
+	    @GetMapping("/ping")
+	    public String ping() {
+	        String mensagem = environment.getProperty("mensagem");
+	        return "ping pong: " + mensagem;
+	    }
+
 }
